83cba2d1ccbbc378d92c87291fbf3528e1550d84
墨楼 <huangzhilin.hzl@antgroup.com>
2026-03-26 18:54:42 +0800

    add code format

---
+++ lua/plugins/python-format.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "isort", "black" },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      for _, tool in ipairs({ "black", "isort" }) do
+        if not vim.tbl_contains(opts.ensure_installed, tool) then
+          table.insert(opts.ensure_installed, tool)
+        end
+      end
+    end,
+  },
+}
